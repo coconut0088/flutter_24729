6c8fb92ff30ee8aa81c368fb4e2b7638c9eea1a8
coconut0088 <sfh2420305@sonline20.sen.go.kr>
2024-07-29 17:53:47 +0900

    무언가 엄청난 것을 만듦

---
+++ lib/stack.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("안녕하세요.")),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: Colors.grey,
+        child: Stack(
+          children: [
+            Container(
+              width: 150,
+              height: 200,
+              color: Colors.brown,
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              margin: const EdgeInsets.only(top: 50, left: 50),
+              color: Colors.lightBlue,
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              margin: const EdgeInsets.only(top: 100, left: 100),
+              color: Colors.green,
+            ),
+            Container(
+              width: 150,
+              height: 200,
+              margin: const EdgeInsets.only(top: 150, left: 150),
+              color: Colors.orange,
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
